22b4ff674b43544b9fb7a9ee73580e077df130a7
Michał Gold <bond.gold@wp.pl>
2022-05-18 01:40:52 +0200

    Organized User Interface code

--- node_server/public/js/main.js
+++ node_server/public/js/main.js
@@ -1,32 +1,6 @@
 //@ts-check
 
-////////////////// User Interfacee //////////////////
-
-/**@type {HTMLImageElement} */
-const cameraImage = document.querySelector("#camera_image"); // Reference to the HTMLImage object
-
-const avgElement = document.querySelector("#avg");
-const devElement = document.querySelector("#dev");
-const cropSizeElement = document.querySelector("#cropSize");
-/**@type {HTMLElement} */
-const avgColorElement = document.querySelector("#avgColor");
-
-let currentImageDataUrl = null; // Last bounded URL to the image data Blob
-/**@param {Blob} newImageData */
-function replaceImage(newImageData){
-    if(currentImageDataUrl){ // Check if image is already bound to a Blob
-        URL.revokeObjectURL(currentImageDataUrl); // Free the memory from the old Blob
-    }
-    currentImageDataUrl = URL.createObjectURL(newImageData) // Bind new Blob to an URL
-    cameraImage.src = currentImageDataUrl // Set the image URL to the new Blob
-}
-
-function updateImageAnalysis(data){
-    avgElement.innerHTML = data.avg;
-    devElement.innerHTML = data.dev;
-    cropSizeElement.innerHTML = data.cropSize;
-    avgColorElement.style.backgroundColor = `rgb(${Math.floor(data.avg[0])}, ${Math.floor(data.avg[1])}, ${Math.floor(data.avg[2])})`
-}
+initUIElements();
 
 ////////////////// MQTT //////////////////////////
 
@@ -44,7 +18,7 @@ function handleMqttMessage(topic, payload){
         console.log("Received test message:", payload.asString());
     }else if(topic == "img/jpeg"){
         console.log("Received new image");
-        replaceImage(new Blob([payload.asBytes()]));
+        updateCameraImage(new Blob([payload.asBytes()]));
     }else if(topic == "analysis"){
         const data = JSON.parse(payload.asString());
         updateImageAnalysis(data);
---
+++ node_server/public/js/ui.js
@@ -0,0 +1,50 @@
+//@ts-check
+
+// References to all important User Interface controls and widgets
+const UI = {
+    /**@type {HTMLImageElement} */
+    cameraImage: null,
+    imageAnalysis:{
+        /**@type {HTMLElement} */
+        avg: null,
+        /**@type {HTMLElement} */
+        dev: null,
+        /**@type {HTMLElement} */
+        cropSize: null,
+        /**@type {HTMLElement} */
+        avgColor: null,
+    },
+};
+
+function initUIElements(){
+    UI.cameraImage = document.querySelector('#camera_image');
+    UI.imageAnalysis.avg = document.querySelector('#avg');
+    UI.imageAnalysis.dev = document.querySelector('#dev');
+    UI.imageAnalysis.cropSize = document.querySelector('#cropSize');
+    UI.imageAnalysis.avgColor = document.querySelector('#avgColor');
+}
+
+let currentImageDataUrl = null; // Last bounded URL to the image data Blob
+
+/**
+ * @param {Blob} newImageData 
+ */
+function updateCameraImage(newImageData){
+    if(currentImageDataUrl){ // Check if image is already bound to a Blob
+        URL.revokeObjectURL(currentImageDataUrl); // Free the memory from the old Blob
+    }
+    currentImageDataUrl = URL.createObjectURL(newImageData) // Bind new Blob to an URL
+    UI.cameraImage.src = currentImageDataUrl // Set the image URL to the new Blob
+}
+
+// Convert a 3-element float array to a CSS string representing an RGB color
+function rgbColorFromArray(arr){
+    return `rgb(${ arr.map(x => Math.floor(x)).join(',') })`;
+}
+
+function updateImageAnalysis(data){
+    UI.imageAnalysis.avg.innerHTML = data.avg;
+    UI.imageAnalysis.dev.innerHTML = data.dev;
+    UI.imageAnalysis.cropSize.innerHTML = data.cropSize;
+    UI.imageAnalysis.avgColor.style.backgroundColor = rgbColorFromArray(data.avg);
+}
