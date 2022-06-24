85f216df85d3fecce67d690caa3dee41293ad877
RoburLee <robert.woznicaa@gmail.com>
2022-06-24 19:02:45 +0200

    ESP32CAM

---
+++ esp32/mqtt_basic/mqtt_basic.ino
@@ -0,0 +1,140 @@
+#include <WiFi.h>
+#define MQTT_MAX_PACKET_SIZE 20000
+#include <PubSubClient.h>
+#include "esp_camera.h"
+#define CAMERA_MODEL_AI_THINKER
+#include "camera_pins.h"
+// WiFi
+const char *ssid = "Lab408"; // Enter your WiFi name
+const char *password = "Laborat408";  // Enter WiFi password
+
+// MQTT message broker
+const char *mqtt_broker = "192.168.0.243";
+const char *topic = "img/jpeg";
+const char *mqtt_username = "user";
+const char *mqtt_password = "password";
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  // Set software serial baud to 115200;
+  Serial.begin(115200);
+  // connecting to a WiFi network
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Trwa łączenie z siecią WiFi...");
+  }
+  Serial.println("Połączono z siecią WiFi");
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 10000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+//  if (psramFound()) {
+    config.frame_size = FRAMESIZE_QVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+    // config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+//  } else {
+//    config.frame_size = FRAMESIZE_SVGA;
+//    config.jpeg_quality = 12;
+//    config.fb_count = 1;
+//  }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1); // flip it back
+    s->set_brightness(s, 1); // up the brightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  s->set_framesize(s, FRAMESIZE_QVGA);
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+  //connecting to a mqtt broker
+  client.setBufferSize(20000);
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);
+  while (!client.connected()) {
+    String client_id = "esp32-client-";
+    client_id += String(WiFi.macAddress());
+    Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+      Serial.println("Public emqx mqtt broker connected");
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+  client.subscribe(topic);
+}
+void callback(char *topic, byte *payload, unsigned int length) {
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+
+  for (int i = 0; i < length; i++) {
+    Serial.print((char) payload[i]);
+  }
+  Serial.println();
+  Serial.println("-----------------------");
+}
+void loop() {
+  client.loop();
+  camera_fb_t* fb = NULL;
+  fb = esp_camera_fb_get();
+  if (fb == NULL)
+  {
+    Serial.println("Nie dziala :(");
+  }
+  else
+  {
+    Serial.println("Wysyłanie...");
+    bool publisher = client.publish(topic, (const uint8_t*)fb->buf, fb->len, false); // DO OBJAŚNIENIA
+    Serial.println(publisher);
+    Serial.println(fb->len);
+    Serial.println(client.getBufferSize());
+    esp_camera_fb_return(fb);
+  }
+  delay(500);
+}
